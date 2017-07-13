6ba958bf0e4a44290a89ef8cc7e8540162843a94
Isashi <isashimitsui@gmail.com>
2017-07-13 16:12:26 +0200

    working cipher, no graphic

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,25 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
-def encrypt
-	puts "Insert the sentence to encrypt:"
-	sentence = gets.chomp
-	puts "How many shifts?"
-	shifts = gets.chomp.to_i
+string = ""
+shift = 0
+
+def encrypt(sentence, shifts)
+	shifts = shifts.to_i
 	chars = sentence.split('')
 	chars_enc = chars.map do |char| 
 	case char.ord
-		when 65..122  then (char.ord + shifts).chr
-		when 122..256 then (char.ord + shifts - 26).chr
+		when 65..122 then if (char.ord + shifts) > 122 then (char.ord + shifts - 58).chr else (char.ord + shifts).chr end
 		else char
 	end
 	end
 	string_enc = chars_enc.join
-	puts string_enc 
+	string_enc 
+end
+
+def decrypt(sentence, shifts)
+	shifts = shifts.to_i
+	chars = sentence.split('')
+	chars_enc = chars.map do |char| 
+	case char.ord
+		when 65..256 then if (char.ord - shifts) < 65 then (char.ord - shifts +58).chr else (char.ord - shifts).chr end
+		else char
+	end
+	end
+	string_enc = chars_enc.join
+	string_enc 
 end
 
 
-
 get "/" do
-		erb :index
+	erb :index, :locals => {:message => nil}
+end
+
+post "/" do
+	if params["decision"]=="Encrypt" then
+	message = encrypt(params["string"], params["shift"])
+	end
+	if params["decision"]=="Decrypt" then
+	message = decrypt(params["string"], params["shift"])
+	end
+	erb :index, :locals => {:string => string, :shift => shift, :message => message}
+
 end
